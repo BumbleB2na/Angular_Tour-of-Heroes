ad7600d7be77753a74c557ab9076ce100bb01e99
b2 <brendan@mobilewebsmart.com>
2017-03-25 17:08:52 -0600

    Created DashboardComponent for /dashboard view
    Created new DashboardComponent.
    Added /dashboard view.
    Redirect root route to /dashboard

--- tutorial/src/app/app.module.ts
+++ tutorial/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule }   from '@angular/forms';  // <-- NgModel lives here
 import { RouterModule }  from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
-import { HeroService } from './hero.service';
+import { HeroService }  from './hero.service';
 
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
     RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
       {
         path: 'heroes',
         component: HeroesComponent
@@ -21,6 +31,7 @@ import { HeroService } from './hero.service';
   ],
   declarations: [ 
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
   ],
