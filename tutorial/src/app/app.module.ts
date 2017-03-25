4fe30f899a544c078c2cee51b8097dba76be93c6
b2 <brendan@mobilewebsmart.com>
2017-03-25 16:30:23 -0600

    Wrote HeroesComponent
    Relocated "Heroes" concerns within current AppComponent to a separate
    HeroesComponent

--- tutorial/src/app/app.module.ts
+++ tutorial/src/app/app.module.ts
@@ -3,18 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';  // <-- NgModel lives here
 
 import { AppComponent }  from './app.component';
-
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [ 
-      BrowserModule,
-      FormsModule // <-- import the FormsModule before binding with [(ngModel)]
-    ],
+    BrowserModule,
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+  ],
   declarations: [ 
-      AppComponent,
-      HeroDetailComponent
-    ],
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+  ],
+  providers: [
+    HeroService,
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
