7d42a65ac27f9056f0e652f1fba3facedd102eed
b2 <brendan@mobilewebsmart.com>
2017-03-26 05:59:24 -0600

    Added custom RoutingModule
    Refactored roots specified in AppModule in to a custom RoutingModule.

--- tutorial/src/app/app.module.ts
+++ tutorial/src/app/app.module.ts
@@ -1,37 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';  // <-- NgModel lives here
-import { RouterModule }  from '@angular/router';
+import { AppRoutingModule } from "./app-routing.module"; //was: import { RouterModule }  from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
-import { HeroService }  from './hero.service';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule,  // custom routing module class - was: RoutingModule.forRoot([ ...paths... ])
   ],
   declarations: [ 
     AppComponent,
